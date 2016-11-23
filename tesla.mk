aa0d0838b3133b6ac062124903227e36c0f1fcfb
dfuse06 <dfuse06@gmail.com>
2016-11-23 13:16:28 -0600

    Make wifi-tablet

--- tesla.mk
+++ tesla.mk
@@ -6,7 +6,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1536
 
 # Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/common_full_tablet_wifionly.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
